6c6fa8f799f113cdc5e828f6c59d9193bc5d8e2d
MateoManolio <manoliomateo@gmail.com>
2025-12-22 07:23:50 -0300

    🗃️ feat: implement repositories for data persistence and remote fetch

--- lib/src/data/repository/character_repository.dart
+++ lib/src/data/repository/character_repository.dart
@@ -1,19 +1,75 @@
 import '../../domain/contracts/i_character_repository.dart';
 import '../../domain/entities/character.dart';
+import '../datasource/interfaces/i_swapi_service.dart';
+import '../models/character_dto.dart';
 
 class CharacterRepository implements ICharacterRepository {
+  final ISwapiService _swapiService;
+  List<Character>? _cachedCharacters;
+
+  CharacterRepository(this._swapiService);
+
   @override
-  Future<List<Character>> getCharacters() {
-    throw UnimplementedError();
+  Future<List<Character>> getCharacters() async {
+    if (_cachedCharacters != null) return _cachedCharacters!;
+
+    try {
+      final dtos = await _swapiService.getPeople();
+      _cachedCharacters = dtos.map(_mapDtoToEntity).toList();
+      return _cachedCharacters!;
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
-  Future<Character?> getCharacterById(String id) {
-    throw UnimplementedError();
+  Future<Character?> getCharacterById(String id) async {
+    if (_cachedCharacters != null) {
+      final cached = _cachedCharacters!.where((c) => c.id.toString() == id);
+      if (cached.isNotEmpty) return cached.first;
+    }
+
+    try {
+      final dto = await _swapiService.getPerson(int.parse(id));
+      return _mapDtoToEntity(dto);
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
-  Future<List<Character>> getCharactersByQuery(String query) {
-    throw UnimplementedError();
+  Future<List<Character>> getCharactersByQuery(String query) async {
+    final characters = await getCharacters();
+    if (query.isEmpty) return characters;
+
+    return characters
+        .where((c) => c.name.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+  }
+
+  Character _mapDtoToEntity(CharacterDto dto) {
+    return Character(
+      id: dto.id,
+      name: dto.name,
+      height: dto.height,
+      mass: dto.mass,
+      gender: dto.gender,
+      homeworld: dto.homeworld,
+      wiki: dto.wiki,
+      image: dto.image,
+      born: dto.born,
+      bornLocation: dto.bornLocation,
+      died: dto.died,
+      diedLocation: dto.diedLocation,
+      species: dto.species,
+      hairColor: dto.hairColor,
+      eyeColor: dto.eyeColor,
+      skinColor: dto.skinColor,
+      cybernetics: dto.cybernetics,
+      affiliations: dto.affiliations,
+      masters: dto.masters,
+      apprentices: dto.apprentices,
+      formerAffiliations: dto.formerAffiliations,
+    );
   }
 }
--- lib/src/data/repository/favorites_repository.dart
+++ lib/src/data/repository/favorites_repository.dart
@@ -1,29 +1,54 @@
 import 'package:holocron/src/domain/entities/character.dart';
-
 import '../../domain/contracts/i_favorites_repository.dart';
+import '../datasource/interfaces/i_favorites_local_service.dart';
+import '../models/character_model.dart';
 
 class FavoritesRepository implements IFavoritesRepository {
+  final IFavoritesLocalDataSource _localDataSource;
+
+  FavoritesRepository(this._localDataSource);
+
   @override
   Future<void> addFavorite(Character character) {
-    // TODO: implement addFavorite
-    throw UnimplementedError();
+    return _localDataSource.saveFavorite(
+      CharacterModel(
+        id: character.id,
+        name: character.name,
+        height: character.height,
+        mass: character.mass,
+        gender: character.gender,
+        homeworld: character.homeworld,
+        wiki: character.wiki,
+        image: character.image,
+        born: character.born,
+        bornLocation: character.bornLocation,
+        died: character.died,
+        diedLocation: character.diedLocation,
+        species: character.species,
+        hairColor: character.hairColor,
+        eyeColor: character.eyeColor,
+        skinColor: character.skinColor,
+        cybernetics: character.cybernetics,
+        affiliations: character.affiliations,
+        masters: character.masters,
+        apprentices: character.apprentices,
+        formerAffiliations: character.formerAffiliations,
+      ),
+    );
   }
 
   @override
-  Future<List<Character>> getFavorites() {
-    // TODO: implement getFavorites
-    throw UnimplementedError();
+  Future<List<Character>> getFavorites() async {
+    return await _localDataSource.getFavorites();
   }
 
   @override
   Future<bool> isFavorite(String id) {
-    // TODO: implement isFavorite
-    throw UnimplementedError();
+    return _localDataSource.containsFavorite(id);
   }
 
   @override
   Future<void> removeFavorite(String id) {
-    // TODO: implement removeFavorite
-    throw UnimplementedError();
+    return _localDataSource.removeFavorite(id);
   }
 }
