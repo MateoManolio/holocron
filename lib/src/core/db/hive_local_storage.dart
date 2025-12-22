aa8338a4b351f41eabd8cf5c563fa1d94a3bd398
MateoManolio <manoliomateo@gmail.com>
2025-12-22 07:19:28 -0300

    feat: configure hive for local data persistence

---
+++ lib/src/core/db/hive_local_storage.dart
@@ -0,0 +1,38 @@
+import 'package:hive_ce_flutter/hive_flutter.dart';
+import '../interfaces/local_storage.dart';
+
+class HiveLocalStorage implements ILocalStorage {
+  @override
+  Future<void> init() async {
+    await Hive.initFlutter();
+  }
+
+  @override
+  Future<T?> getValue<T>(String boxName, String key) async {
+    final box = await Hive.openBox(boxName);
+    return box.get(key) as T?;
+  }
+
+  @override
+  Future<void> setValue<T>(String boxName, String key, T value) async {
+    final box = await Hive.openBox(boxName);
+    await box.put(key, value);
+  }
+
+  @override
+  Future<void> deleteValue(String boxName, String key) async {
+    final box = await Hive.openBox(boxName);
+    await box.delete(key);
+  }
+
+  @override
+  Future<void> clearBox(String boxName) async {
+    final box = await Hive.openBox(boxName);
+    await box.clear();
+  }
+
+  @override
+  Future<void> close() async {
+    await Hive.close();
+  }
+}
---
+++ lib/src/data/datasource/local/favorites_local_service.dart
@@ -0,0 +1,49 @@
+import '../../../core/interfaces/local_storage.dart';
+import '../../models/character_model.dart';
+import '../interfaces/i_favorites_local_service.dart';
+
+class FavoritesLocalDataSourceImpl implements IFavoritesLocalDataSource {
+  final ILocalStorage _storage;
+  static const String _boxName = 'favorites';
+
+  FavoritesLocalDataSourceImpl(this._storage);
+
+  @override
+  Future<List<CharacterModel>> getFavorites() async {
+    final favorites = await _storage.getValue<List<dynamic>>(_boxName, 'list');
+    if (favorites == null) return [];
+
+    return favorites
+        .map(
+          (e) => CharacterModel.fromJson(Map<String, dynamic>.from(e as Map)),
+        )
+        .toList();
+  }
+
+  @override
+  Future<void> saveFavorite(CharacterModel character) async {
+    final favorites = await getFavorites();
+    if (!favorites.any((e) => e.id == character.id)) {
+      favorites.add(character);
+      await _saveList(favorites);
+    }
+  }
+
+  @override
+  Future<void> removeFavorite(String id) async {
+    final favorites = await getFavorites();
+    favorites.removeWhere((e) => e.id.toString() == id);
+    await _saveList(favorites);
+  }
+
+  @override
+  Future<bool> containsFavorite(String id) async {
+    final favorites = await getFavorites();
+    return favorites.any((e) => e.id.toString() == id);
+  }
+
+  Future<void> _saveList(List<CharacterModel> list) async {
+    final jsonList = list.map((e) => e.toJson()).toList();
+    await _storage.setValue(_boxName, 'list', jsonList);
+  }
+}
